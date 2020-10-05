bdefb8ec502724a0fae08c8ff646865b6be5e45c
Opilane TTHK <opilane@tthk.ee>
2020-10-05 14:49:29 +0300

    zombiegame

---
+++ WardrobePicker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WardrobePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wardrobeClothes = { "hoodie", "shirt", "jeans", "hat", "underwear" };
+            string[] wardrobeColors = { "red", "green", "blue", "black", "yellow" };
+            string[] wardrobePatterns = { "flowery", "striped", "polka dot", "circles", "checkered" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, wardrobeClothes.Length);
+            int randomIndex2 = rnd.Next(0, wardrobeColors.Length);
+            int randomIndex3 = rnd.Next(0, wardrobePatterns.Length);
+
+            Console.WriteLine("Do you need any assistance on what to take from the wardrobe?");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "no")
+            {
+                Console.WriteLine("Farewell.");
+            }else if(answer == "yes")
+            {
+                Console.WriteLine($"Today you will wear {wardrobeColors[randomIndex2]} {wardrobePatterns[randomIndex3]} {wardrobeClothes[randomIndex]}");
+            }else
+            {
+                Console.WriteLine("Sadly I cannot help you.");
+            }
+
+        }
+    }
+}
---
+++ WhatsInTheBag/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Dynamic;
+
+namespace WhatsInTheBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bagItems = new string[5];
+            int i = 0;
+            while (i < bagItems.Length)
+            {
+                Console.WriteLine("What's in the bag?");
+                bagItems[i] = Console.ReadLine();
+                i++;
+            }
+
+            Random rnd = new Random();
+
+            Console.WriteLine($"You will have to fight the zombies with a {bagItems[rnd.Next(0, bagItems.Length)]}");
+        }
+    }
+}
